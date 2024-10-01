d4ce56e533d1453ba384c9772d003b687e86104b
BT_2S14_34 <leo1281515@gmail.com>
2024-10-01 10:48:47 -0400

    Initial commit

---
+++ src/DesignDocument.java
@@ -0,0 +1,25 @@
+//File:LinearEquation.java
+//
+//LinearEquation Class
+//Instance Variables to use:
+//private int x1
+//private int y1
+//private int x2
+//private int y2
+
+//Constructor
+//  public linearEquation(int x1, int y1, int x2, int y2)
+//     State variables here
+
+//Methods
+
+//   public findSlope(int x1, int x2, int y1, int y2)
+//            return (double) (y2 - y1)/ (x2 - x1)
+
+//Distance formula sqrt (pow 2 (x2 - x1)  + pow 2 (y2 - y1))
+//Math.sqrt(((Math.pow(x2 - x1)) + (Math.pow(y2 - y1)))
+
+//Find y intercept:
+//
+
+
