163f8bf699518516304bbc52e8b474e2219125a8
BT_2S14_34 <leo1281515@gmail.com>
2024-10-01 11:04:12 -0400

    Added on y intercept and what can be printed in Main.

--- src/DesignDocument.java
+++ src/DesignDocument.java
@@ -13,13 +13,16 @@
 
 //Methods
 
-//   public findSlope(int x1, int x2, int y1, int y2)
-//            return (double) (y2 - y1)/ (x2 - x1)
-
 //Distance formula sqrt (pow 2 (x2 - x1)  + pow 2 (y2 - y1))
-//Math.sqrt(((Math.pow(x2 - x1)) + (Math.pow(y2 - y1)))
+//Math.sqrt(((Math.pow((x2 - x1), 2) + (Math.pow((y2 - y1), 2))
+
 
 //Find y intercept:
-//
+// slope = (double) (y2 - y1)/ (x2 - x1)
+// yInter = (slope * x1) - y1
 
+
+//On Main
+//String slopeInter = "Slope intercept form: y = %x        "
+//Use strting Format
 
