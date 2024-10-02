e58062fbdcb3349974976fc404e4f184c8e6c5b5
BT_2S14_34 <leo1281515@gmail.com>
2024-10-02 11:04:56 -0400

    Added comments to the Runner class to define what I did in the code.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -9,7 +9,9 @@ public class EquationRunner {
 
         System.out.print("Enter the second coordinate point (x2,y2): ");
         String secondPoint = scanner.nextLine();
+        //from the first index it would take the rest of the following value up to the comma.
         double x2 = Double.parseDouble(secondPoint.substring(1, secondPoint.indexOf(',')));
+        //takes the index of the comma and takes the value of the next index to the last index excluding the parenthesis.
         double y2 = Double.parseDouble(secondPoint.substring(secondPoint.indexOf(',') + 1, secondPoint.length() - 1));
 
         LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
