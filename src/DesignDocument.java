97d81b8407761f946737df8fd75d0cb3630d8f31
Leo <leo1281515@gmail.com>
2024-10-01 23:19:51 -0400

    Added more methods for the planning file (include slope, y inter and distance.

--- src/DesignDocument.java
+++ src/DesignDocument.java
@@ -2,25 +2,37 @@
 //
 //LinearEquation Class
 //Instance Variables to use:
-//private int x1
-//private int y1
-//private int x2
-//private int y2
+//private double x1
+//private double y1
+//private double x2
+//private double y2
+//double slope
+//double yIntercept
+//double distance
 
 //Constructor
-//  public linearEquation(int x1, int y1, int x2, int y2)
+//  public linearEquation(double x1, double x2, double x2, double y2)
 //     State variables here
 
 //Methods
 
+//public double calculateDistance()
+
+//Returns the distance between the two points, rounded to the nearest hundredth.
 //Distance formula sqrt (pow 2 (x2 - x1)  + pow 2 (y2 - y1))
 //Math.sqrt(((Math.pow((x2 - x1), 2) + (Math.pow((y2 - y1), 2))
 
+//public double calculateSlope()
 
-//Find y intercept:
+//Returns: The slope of the line, rounded to the nearest hundredth.
+//public double calculateYIntercept()
+
+//Returns: The y-intercept of the line, rounded to the nearest hundredth.
 // slope = (double) (y2 - y1)/ (x2 - x1)
 // yInter = (slope * x1) - y1
 
+
+//public String toString()
 
 //On Main
 //String slopeInter = "Slope intercept form: y = %x        "
