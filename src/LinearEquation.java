82b0d3748870aaf0472a12ac90b4612bfd23b7b2
BT_2S14_34 <leo1281515@gmail.com>
2024-10-07 09:52:35 -0400

    Added more comments to the LinearEquation class to explain how String.format works.

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -12,38 +12,30 @@ public LinearEquation(double x1, double x2, double y1, double y2){
     this.x2 = x2;
     this.y1 = y1;
     this.y2 = y2;
-    calculateDistance();
-    calculateSlope();
-    calculateYIntercept();
-    fractionForm();
-
 }
 
-public double calculateDistance(){
+public double distance(){
     return Math.round(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow((y2 - y1), 2)));
 }
 
-public double calculateSlope() {
+public double slope() {
     return Math.round((y2 - y1) / (x2 - x1));
 }
 
-public double calculateYIntercept() {
+public double yIntercept() {
     return Math.round((y1 - slope * x1));
 
 }
 
+//".0" Assures that there is no digits after the decimal point, while f represents floating point number.
 public String fractionForm() {
     return String.format("%.0f/1", slope);
 
 }
 
+//This would
 public String toString(){
-    return String.format("y = " + fractionForm() + "");
+    return String.format("y = " + fractionForm() + "" + yIntercept);
 }
 
-//public int fractionCheck(){
-//    String str = fractionForm();
-//    String splitStr = str.split(",");
-//    if str.
-//}
 }
