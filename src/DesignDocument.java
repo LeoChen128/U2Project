5cc674ce55990b31c98e3d161f0138cae86c2c48
Leo <leo1281515@gmail.com>
2024-10-15 19:04:54 -0400

    Added and changed decimalFormSlope and converted slope into fraction form.

--- src/DesignDocument.java
+++ src/DesignDocument.java
@@ -2,10 +2,7 @@
 //
 //LinearEquation Class
 //Instance Variables to use:
-//private double x1
-//private double y1
-//private double x2
-//private double y2
+//x1,y1,x2,y2
 //double slope
 //double yIntercept
 //double distance
@@ -22,7 +19,7 @@
 //Distance formula sqrt (pow 2 (x2 - x1)  + pow 2 (y2 - y1))
 //Math.sqrt(((Math.pow((x2 - x1), 2) + (Math.pow((y2 - y1), 2))
 
-//public double calculateSlope()
+//public double slope()
 
 //Returns: The slope of the line, rounded to the nearest hundredth.
 //public double calculateYIntercept()
@@ -31,15 +28,15 @@
 // slope = (double) (y2 - y1)/ (x2 - x1)
 // yInter = (slope * x1) - y1
 
+//public String fractionFormSlope()
+//convert slope into fraction form and use if statements to change signs
 
 //conditions on fractions
 //if numerator % denominator = 0; do numerator/denominator
-//if numerator * denominator > 0 then pos; if not the negative
+//if numerator * denominator > 0 then pos; if not then negative
 
 
 //public String toString()
-
-//On Main
-//String slopeInter = "Slope intercept form: y = %x        "
-//Use strting Format
+//Prints out the following information:
+//first pair, second pair, slope, y inter, equation of line, distance
 
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,49 +3,61 @@ public class LinearEquation {
     private double y1;
     private double x2;
     private double y2;
-    double slope;
 
 public LinearEquation(double x1, double x2, double y1, double y2){
     this.x1 = x1;
     this.x2 = x2;
     this.y1 = y1;
     this.y2 = y2;
+
 }
 
 public double distance(){
+
     return Math.round(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow((y2 - y1), 2)));
 }
 
 public double slope() {
-    return Math.round((y2 - y1) / (x2 - x1));
+    return Math.round((y2 - y1) / (x2 - x1) * 100) / 100.0;
 }
 
 public double yIntercept() {
-    return Math.round((y1 - slope * x1));
+    double slope = slope();
+    return Math.round(((slope * x1) - y1) * 100) / 100.0;
 
 }
 
 //".0" Assures that there is no digits after the decimal point, while f represents floating point number.
-public String fractionForm() {
+public String fractionFormSlope() {
+    double slope = slope();
+    if (slope < 0){
+        Math.abs(slope());
+        return String.format("- %.0f/1", slope);
+    }
+    if (slope > 0){
+        return String.format("+ %.0f/1", slope);
+    }
     return String.format("%.0f/1", slope);
 
 }
-
-//This would
-public String toString(){
+public String decimalFormYInter(){
     if ((yIntercept()) > 0) {
-        String formatYInter = String.format("+ %.2f", yIntercept());
-        return String.format("y = " + fractionForm() + " " + yIntercept());
+        return String.format("+ %.2f", yIntercept());
     }
 
     else if ((yIntercept()) < 0){
         Math.abs(yIntercept());
-        String formatYInter = String.format("- %.2f", yIntercept());
-        return String.format("y = " + fractionForm() + " " + yIntercept());
+        return String.format("- %.2f", yIntercept());
     }
+    return String.format("%.2f", yIntercept());
+}
 
+public String toString(){
+    String yInter = decimalFormYInter();
+    String slope = fractionFormSlope();
 
-    return String.format("y = " + fractionForm() + " " + yIntercept());
+    return "y = " + fractionFormSlope() + "x " + yInter + "/n"
+            + "Slope: " + slope + "x /n";
 }
 
 
